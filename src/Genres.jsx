5b503fff70793ac02e5e5e7f5f39d7280a99ccb6
localhostwiththemost <jpowelldev@gmail.com>
2023-02-08 14:55:07 -0600

    Added logic that fetches the specific genre the user clicks and a corresponding button to return them back to the list of genres. Added favicons as well.

--- src/Genres.jsx
+++ src/Genres.jsx
@@ -11,6 +11,7 @@ function Genres() {
   const [genre, setGenre] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [displayGenres, setDisplayGenres] = useState(true);
 
   useEffect(() => {
     setLoading(false);
@@ -32,57 +33,163 @@ function Genres() {
 
   return (
     <>
-      <section className="results-section">
-        <ul className="genre-link__container">
-          <li>
-            <a onClick={() => setGenre("action")}>Action</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("adventure")}>Adventure</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("comedy")}>Comedy</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("crime")}>Crime</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("documentary")}>Documentary</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("drama")}>Drama</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("family")}>Family</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("fantasy")}>Fantasy</a>
-          </li>
-          <li>
-            <a onClick={() => setGenre("horror")}>Horror</a>
-          </li>
-        </ul>
-
-        {loading ? (
-          <div className="container">
-            <div className="empty">
-              <Loader />
-            </div>
-          </div>
-        ) : movies.length > 0 ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="container">
-            <div className="empty">
-              <h2>No movies found</h2>
-            </div>
-          </div>
-        )}
-      </section>
+      {displayGenres ? (
+        <section className="genres-section__empty">
+          <ul className="genre-link__container">
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("action");
+                  setDisplayGenres(false);
+                }}
+              >
+                Action
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("adventure");
+                  setDisplayGenres(false);
+                }}
+              >
+                Adventure
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("comedy");
+                  setDisplayGenres(false);
+                }}
+              >
+                Comedy
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("crime");
+                  setDisplayGenres(false);
+                }}
+              >
+                Crime
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("documentary");
+                  setDisplayGenres(false);
+                }}
+              >
+                Documentary
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("drama");
+                  setDisplayGenres(false);
+                }}
+              >
+                Drama
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("family");
+                  setDisplayGenres(false);
+                }}
+              >
+                Family
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("fantasy");
+                  setDisplayGenres(false);
+                }}
+              >
+                Fantasy
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setGenre("horror");
+                  setDisplayGenres(false);
+                }}
+              >
+                Horror
+              </a>
+            </li>
+          </ul>
+        </section>
+      ) : (
+        <>
+          {loading ? (
+            <>
+              <section className="genres-section__empty">
+                <ion-icon
+                  name="arrow-back-outline"
+                  className="back-icon"
+                  onClick={() => {
+                    setGenre("");
+                    setDisplayGenres(true);
+                  }}
+                ></ion-icon>
+                <div className="container">
+                  <div className="empty">
+                    <Loader />
+                  </div>
+                </div>
+              </section>
+            </>
+          ) : movies.length > 0 ? (
+            <>
+              <section className="genres-section">
+                <ion-icon
+                  name="arrow-back-outline"
+                  className="back-icon"
+                  onClick={() => {
+                    setGenre("");
+                    setDisplayGenres(true);
+                  }}
+                ></ion-icon>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div className="container">
+                  {movies.map((movie) => (
+                    <MovieCard movie={movie} />
+                  ))}
+                </div>
+              </section>
+            </>
+          ) : (
+            <>
+              <section className="genres-section__empty">
+                <ion-icon
+                  name="arrow-back-outline"
+                  className="back-icon"
+                  onClick={() => {
+                    setGenre("");
+                    setDisplayGenres(true);
+                  }}
+                ></ion-icon>
+                <div className="container">
+                  <div className="empty">
+                    <h2>No movies found</h2>
+                  </div>
+                </div>
+              </section>
+            </>
+          )}
+        </>
+      )}
     </>
   );
 }
